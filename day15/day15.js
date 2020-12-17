6bfc27c9d82242c4469f2c2b969194eee268e0a7
Alvaro Serrano <alsersan20@gmail.com>
2020-12-17 15:17:16 +0100

    day 15 solutions

---
+++ day15/day15.js
@@ -0,0 +1,51 @@
+const testInput1 = `0,3,6`; // Answer P1: 436  |  Answer P2:
+const testInput2 = `1,3,2`; // Answer P1: 1    |  Answer P2:
+const testInput3 = `2,1,3`; // Answer P1: 10   |  Answer P2:
+const testInput4 = `1,2,3`; // Answer P1: 27   |  Answer P2:
+const testInput5 = `2,3,1`; // Answer P1: 78   |  Answer P2:
+const testInput6 = `3,2,1`; // Answer P1: 438  |  Answer P2:
+const testInput7 = `3,1,2`; // Answer P1: 1836 |  Answer P2:
+const input = `16,11,15,0,1,7`;
+
+const parseInput = (input) => {
+  return input.split(",").map((el) => parseInt(el, 10));
+};
+
+// PART 1, PART 2
+
+const resolve = (input, count) => {
+  const numbers = parseInput(input);
+  const turns = new Map();
+
+  let lastNum = 0;
+  for (let i = 0; i < count; i++) {
+    if (i < numbers.length) {
+      if (i === 0) {
+        lastNum = numbers[i];
+      } else {
+        turns.set(numbers[i - 1], i - 1);
+        lastNum = numbers[i];
+      }
+    } else {
+      if (turns.has(lastNum)) {
+        const currentNum = i - 1 - turns.get(lastNum);
+        turns.set(lastNum, i - 1);
+        lastNum = currentNum;
+      } else {
+        turns.set(lastNum, i - 1);
+        lastNum = 0;
+      }
+    }
+  }
+  return lastNum;
+};
+
+console.time("Part1");
+const answer1 = resolve(input, 2020);
+console.timeEnd("Part1");
+console.log(`Answer Part 1: ${answer1}`);
+
+console.time("Part2");
+const answer2 = resolve(input, 30000000);
+console.timeEnd("Part2");
+console.log(`Answer Part 2: ${answer2}`);
